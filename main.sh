c66d95f2abc0deafc6c2a503ce2692a57445e046
nyeees <98702857+nyeees@users.noreply.github.com>
2022-10-11 10:22:08 +0200

    Ordenar notas por...

--- main.sh
+++ main.sh
@@ -109,6 +109,7 @@ function helpPanel(){
 }
 function ctrl_c(){
 	echo -e "${redColour}[!]Saliendo...${endColour}" 
+	(rm miasignatura.txt  asignaturas.txt notas.txt table.txt )  2>/dev/null
 	exit 1
 }
 
@@ -130,37 +131,50 @@ function getCookie(){
 function notasInfo(){
 
 
-	if [ "$year" == "all" ];then
-		echo -e "Buscar por cualquier año"
+	if [ "$option" == "nota" ];then
+
+		echo -e "\n\t${grayColour}A continuacion se ordenara por ${endColour}${yellowColour}nota${endColour}"
+	elif [ "$option" == "percentil" ];then
+		echo -e "\n\t${grayColour}A continuacion se ordenara por ${endColour}${yellowColour}percentil ${endColour}"
+	elif [ "$option" == "suspendidos" ];then
+		echo -e "\n\t${grayColour}A continuacion se ordenara por ${endColour}${yellowColour}suspendidos${endColour}"
 	else
-		echo -e "Buscas por el año ${year}"
+		echo -e "\n\t${redColour}Opcion incorrecta.[!]Saliendo...${endColour}\n"
+		exit 1
+
 	fi
 
+	cat notas.txt | grep "Unica " -B 1 | grep -v "Uni" | tr -d '(0123456789)' > asignaturas.txt
+	asignaturas=$(cat notas.txt | grep -i "Unica " -A 1 | grep -vE "Unic" | tr -d '(0123456789)')
 
-	asignaturas=$(cat notas.txt | grep -i "Unica " -A 1 | grep -vE "Unic" | tr -d '0123456789' | tr -d '()')
 	echo -e "${grayColour}"
 	echo "Año_Asignatura_Nota_Percentil_Suspensos" > table.txt
-	
+
 	while read asignatura ;do 
+					
 		
-		notaArray+=($(cat notas.txt | grep -m 1  "${asignatura}" -A 1 | tail -n 1 | awk '{print$4}'))
-		percentilArray+=($(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignatura}" -A 1 | tail -n 1 | awk '{print$3}'))
-		#suspensosArray+=($(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignatura}" -A 1 | tail -n 1 | awk '{print$7}'))
+		notaArray+=($(cat notas.txt | grep -m 1  "${asignatura} " -A 1 | tail -n 1 | awk '{print$4}'))
+		percentilArray+=($(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignatura} " -A 1 | tail -n 1 | awk '{print$3}'))
+		aprobadosArray+=($(cat notas.txt | grep -i "suspendidos" -A 100 | grep "${asignatura} " -A 1 | tail -n 1 |awk '{print$6}'))
+		suspensosArray+=($(cat notas.txt | grep -i "suspendidos" -A 100 | grep "${asignatura} " -A 1 | tail -n 1 |awk '{print$7}'))
 	done < asignaturas.txt
-	
+		
+		sortedSuspensos=($(printf '%s\n' ${suspensosArray[@]} | sort -n)) 
+		
 		IFS=$'\n' sortedNota=($(sort <<<"${notaArray[*]}"));
 		IFS=$'\n' sortedPercentil=($(sort <<<"${percentilArray[*]}"));
-		IFS=$'\n' sortedSuspensos=($(sort <<<"${suspensosArray[*]}"));
+		IFS=$'\n' sortedAprobados=($(sort <<<"${aprobadosArray[*]}"));
 		unset IFS
+			
+		
+		
 
 		declare -i counter=0
 		declare -i counterAsignatura=1
-
+		
 	while read asignatura ;do
-		opcion=notas
 	
-		if [ "$opcion" == "notas" ];then	
-			let counter+=1
+		if [ "$option" == "nota" ];then	
 			if [ "${sortedNota[counter]}" == ${sortedNota[counter-1]} ]; then
 				let counterAsignatura+=1
 			else
@@ -168,26 +182,51 @@ function notasInfo(){
 			fi
 			miAsignatura=$(cat notas.txt | grep -i "Unica " -B 1 | grep "${sortedNota[counter]}" -B 1 | grep -vE "\-|Unica" | tr -d '(0123456789)'| sed -n ${counterAsignatura}p)
 
-			echo "$(cat notas.txt | grep -i "Unica " -B 1 | grep "${sortedNota[counter]}" -B 1 | grep "^[[:digit:]]" | awk '{print$1}' | sed -n ${counterAsignatura}p)_$(cat notas.txt | grep -i "Unica " -B 1 | grep "${sortedNota[counter]}" -B 1 | grep -vE "\-|Unica" | tr -d '(0123456789)'| sed -n ${counterAsignatura}p)_${sortedNota[counter]}_$(cat notas.txt | grep -i "$miAsignatura" -A 1 | tail -n 1 | awk '{print$3}')_$(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignatura}" -A 1 | tail -n 1 | awk '{print$7}')" >> table.txt
+			echo "$(cat notas.txt | grep -i "Unica " -B 1 | grep "${sortedNota[counter]}" -B 1 | grep "^[[:digit:]]" | awk '{print$1}' | sed -n ${counterAsignatura}p)_$(cat notas.txt | grep -i "Unica " -B 1 | grep "${sortedNota[counter]}" -B 1 | grep -vE "\-|Unica" | tr -d '(0123456789)'| sed -n ${counterAsignatura}p)_${sortedNota[counter]}_$(cat notas.txt | grep -i "$miAsignatura" -A 1 | tail -n 1 | awk '{print$3}')_$(cat notas.txt | grep -i "$miAsignatura" -A 1 | tail -n 1 | awk '{print$7}')" >> table.txt
 		#cat table.txt
 	
-		else
+		elif [ "$option" == "año" ];then
 			
 			echo "$(cat notas.txt | grep -m 1  "${asignatura}" -A 1 | tail -n 1 | awk '{print$1}')_${asignatura}_$(cat notas.txt | grep -m 1  "${asignatura}" -A 1 | tail -n 1 | awk '{print$4}')_$(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignatura}" -A 1 | tail -n 1 | awk '{print$3}')_$(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignatura}" -A 1 | tail -n 1 | awk '{print$7}')" >> table.txt
+
+		elif [ "$option" == "percentil" ];then
+
+			if [ "${sortedPercentil[counter]}" == "${sortedPercentil[counter-1]}" ]; then
+                        	let counterAsignatura+=1
+                        else
+                        	counterAsignatura=1
+                        fi
+			
+			asignaturaPerc=$(cat notas.txt | grep "Asignatura .* ${sortedPercentil[counter]} " -B 1 | grep -vE "Asig|\-" | tr -d '(0123456789)' | sed -n ${counterAsignatura}p)
+			echo "$(cat notas.txt | grep -m 1  "${asignatura}" -A 1 | tail -n 1 | awk '{print$1}')_${asignaturaPerc}_$(cat notas.txt | grep -m 1  "${asignaturaPerc}" -A 1 | tail -n 1 | awk '{print$4}')_${sortedPercentil[counter]}_$(cat notas.txt | grep   -i "Alumnos percentil" -A 100 | grep "${asignaturaPerc}" -A 1 | tail -n 1 | awk '{print$7}')" >> table.txt	
+
+		elif [ "$option" == "suspendidos" ];then
+			if [ "${sortedSuspensos[counter]}" == "${sortedSuspensos[counter-1]}" ]; then
+                                  let counterAsignatura+=1
+                          else
+                                  counterAsignatura=1
+                          fi
+			asignaturaSusp=$(cat notas.txt | grep "^Asignatura .*  ${sortedSuspensos[counter]}" -B 1 | grep "% .* ${sortedSuspensos[counter]}" -B 1 | grep -vE "Asi|\-" | tr -d '(0123456789)' | sed -n ${counterAsignatura}p)
+
+
+			echo "$(cat notas.txt | grep -m 1  "${asignaturaSusp}" -A 1 | tail -n 1 | awk '{print$1}')_${asignaturaSusp}_$(cat notas.txt | grep -m 1  "${asignaturaSusp}" -A 1 | tail -n 1 | awk '{print$4}')_$(cat notas.txt | grep -i "Alumnos percentil" -A 100 | grep "${asignaturaSusp}" -A 1 | tail -n 1 | awk '{print$3}')_${sortedSuspensos[counter]}" >> table.txt
+		
 		fi
+	let counter+=1
 	done < asignaturas.txt
 		
 	printTable '_' "$(cat table.txt)"
 	echo -e "${endColour}"
-	rm miasignatura.txt
+	(rm miasignatura.txt  asignaturas.txt notas.txt table.txt )  2>/dev/null
 }
 
 
 declare -i parameter_counter=0
-while getopts "a:ng:c:h" arg; do
+while getopts "a:ng:c:o:h" arg; do
 	case $arg in
 		c)cookie_in=$OPTARG;;
 		n)let parameter_counter+=2;;
+		o)option=$OPTARG;;
 		a)year=$OPTARG;;
 		g)userPass=($OPTARG);let parameter_counter+=3;; #Escribe el nombre y contra entre ""
 		h);;
@@ -195,11 +234,16 @@ while getopts "a:ng:c:h" arg; do
 done
 
 
+
+
 if [ $parameter_counter -eq 2 ];then
-	if [ "$cookie_in" ];then
+	curl -s --cookie "TDp=$cookie_in" -d "p_curso=1" "https://intranet.upv.es/pls/soalu/sic_asi.notes_temaalu_asi" | html2text > notas.txt
+	(cat notas.txt | grep -i "suspendidos") &>/dev/null
+	
+	if [ "$(echo $?)" -eq 0 ];then
 		notasInfo
 	else
-		echo -e "${redColour}\nError ${endColour}${grayColour}Proporciona una cookie de sesion. Si no la tienes usa el parametro -g "
+		echo -e "${redColour}\nError ${endColour}${grayColour}Proporciona una cookie de sesion valida. Si no la tienes usa el parametro -g "
 	fi
 elif [ $parameter_counter -eq 3 ];then
 	getCookie
